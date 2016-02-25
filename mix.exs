2e193fddd7bf209214b982f323498b2ae0dbf8f4
Jay Hayes <ur@iamvery.com>
2016-02-24 18:41:09 -0600

    Include hound app in test env

--- mix.exs
+++ mix.exs
@@ -18,8 +18,11 @@ defmodule PinsterPhoenix.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {PinsterPhoenix, []},
-     applications: apps]
+     applications: apps(Mix.env)]
   end
+
+  def apps(:test), do: [:hound | apps]
+  def apps(_), do: apps
 
   def apps do
     [:phoenix, :phoenix_html, :cowboy, :logger, :gettext, :phoenix_ecto, :postgrex]
