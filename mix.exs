3fe7daeb18dcd319357867d302b75cde9f976fd3
Jay Hayes <ur@iamvery.com>
2016-02-24 18:40:06 -0600

    Add hound dependency

--- mix.exs
+++ mix.exs
@@ -38,6 +38,7 @@ defmodule PinsterPhoenix.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.9"},
       {:cowboy, "~> 1.0"},
+      {:hound, "~> 0.8", only: :test},
     ]
   end
 
