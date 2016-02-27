04daddf5fd2c10883a7de3363092bcfdeeaef34d
Jay Hayes <ur@iamvery.com>
2016-02-27 09:00:21 -0600

    Move the db config from the secret file to prod config
    Configure it to get the db information from the prod (Heroku)
    environment.

--- config/prod.exs
+++ config/prod.exs
@@ -20,6 +20,12 @@ config :pinster_phoenix, PinsterPhoenix.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Configure your database
+config :pinster_phoenix, PinsterPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
