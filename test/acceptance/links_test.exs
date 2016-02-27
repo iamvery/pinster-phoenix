d27628c430850245d7fcfd00b9ec8bcb115d9dc9
Jay Hayes <ur@iamvery.com>
2016-02-26 18:50:06 -0600

    Implement links index

--- test/acceptance/links_test.exs
+++ test/acceptance/links_test.exs
@@ -3,4 +3,16 @@ defmodule PinsterPhoenix.LinksTest do
   use Hound.Helpers
 
   hound_session
+
+  alias PinsterPhoenix.Link
+
+  test "displays all links" do
+    Link.changeset(%Link{}, %{url: "http://iamvery.com"}) |> Repo.insert!
+    Link.changeset(%Link{}, %{url: "http://google.com"}) |> Repo.insert!
+
+    navigate_to "/links"
+
+    assert page_source =~ "iamvery.com"
+    assert page_source =~ "google.com"
+  end
 end
--- web/controllers/link_controller.ex
+++ web/controllers/link_controller.ex
@@ -1,3 +1,10 @@
 defmodule PinsterPhoenix.LinkController do
   use PinsterPhoenix.Web, :controller
+
+  alias PinsterPhoenix.Link
+
+  def index(conn, _params) do
+    links = Repo.all(Link)
+    render conn, "index.html", links: links
+  end
 end
