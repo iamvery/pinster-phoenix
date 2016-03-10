1ad747b20e0a1daf432335912807393cde5792ff
Jay Hayes <ur@iamvery.com>
2016-03-10 13:05:52 -0600

    Revert "Extract function to list apps"
    This reverts commit ea7a9c9d29fdabfbb7af3bd8ee611fead751fa9e.

--- mix.exs
+++ mix.exs
@@ -18,11 +18,8 @@ defmodule PinsterPhoenix.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {PinsterPhoenix, []},
-     applications: apps]
-  end
-
-  def apps do
-    [:phoenix, :phoenix_html, :cowboy, :logger, :gettext, :phoenix_ecto, :postgrex]
+     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext,
+                    :phoenix_ecto, :postgrex]]
   end
 
   # Specifies which paths to compile per environment.
